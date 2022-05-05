91222973b0d69e83a1953cd29fb948f1b3832bd3
Brice Videau <bvideau@anl.gov>
2022-05-05 13:57:59 -0500

    Updating lttng versions, and add liburcu as versions are outdated on spack.

--- packages/lttng-ust/package.py
+++ packages/lttng-ust/package.py
@@ -14,10 +14,13 @@ class LttngUst(AutotoolsPackage):
 
     maintainers = ['Kerilk']
 
+    version('2.13.2', sha256='08679a1dfc2c9428885273861628aa5d828972f389a38bb08fdce39adee589b7')
+    version('2.12.4', sha256='2124da2003a921f5da86c9aec00b897b5bbc006b0110a3ab29f1c1bc1c073f86')
     version('2.12.0', sha256='1983edb525f3f27e3494088d8d5389b4c71af66bbfe63c6f1df2ad95aa44a528')
     version('2.11.2', sha256='6b481cec7fe748503c827319e3356137bceef4cce8adecbda3a94c6effcdd161')
     version('2.10.7', sha256='a9c651eea8a33f50c07a6e69e3e4094e4897340c97eb0166e6dde0e80668742b')
 
-    depends_on('userspace-rcu@0.11:')
+    depends_on('userspace-rcu@0.12:', when='@2.13:')
+    depends_on('userspace-rcu@0.11:', when=':@2.12.999')
     depends_on('numactl')
     depends_on('pkg-config')
