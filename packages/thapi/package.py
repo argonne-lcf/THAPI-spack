d0c49e61075f5a9289ce3403733c14497394e95b
Thomas Applencourt <apl@anl.gov>
2024-07-22 16:19:31 +0000

    add variant

--- packages/thapi/package.py
+++ packages/thapi/package.py
@@ -44,6 +44,7 @@ class Thapi(AutotoolsPackage):
     # We don't add Python as a runtime dependency of lttng to avoid python propagated as a runtime dependency of thapi
     depends_on('python', type=('build'))
 
+    variant('archive', default=False, description='Support for live analysis of archive')
     with when("+archive"):
         depends_on('babeltrace2@archive', type=('build', 'run'))
 
