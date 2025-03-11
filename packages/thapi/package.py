acc7b201087c2fa845bf87e5f5a8462c7835e516
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-03-11 21:47:10 +0000

    Add a check for compiler versions

--- packages/thapi/package.py
+++ packages/thapi/package.py
@@ -22,6 +22,8 @@ class Thapi(AutotoolsPackage):
     version('0.0.8', tag='v0.0.8')
     version('0.0.7', tag='v0.0.7')
 
+    depends_on("c", type=("build"))
+    depends_on("cxx", type=("build"))
     depends_on('automake', type=('build'))
     depends_on('autoconf', type=('build'))
     depends_on('libtool', type=('build'))
@@ -34,6 +36,12 @@ class Thapi(AutotoolsPackage):
     depends_on('lttng-tools', type=('build', 'link', 'run'), when='@0.0.8:')
     depends_on('lttng-ust@:2.12.999', type=('build', 'link', 'run'), when='@:0.0.7')
     depends_on('lttng-tools@:2.12.999', type=('build', 'link', 'run'), when='@:0.0.7')
+
+    # Check compilers and versions. Version hecks are mainly for magic_enum:
+    # https://github.com/Neargye/magic_enum?tab=readme-ov-file#compiler-compatibility
+    conflicts("%gcc@:8", msg="GCC version >= 9 required.")
+    conflicts("%llvm@:4", msg="clang >= 5 required.")
+    conflicts("%msvc", msg="MSVC is not supported.")
 
     # Restricting to ruby <= 3.1 when spack is less than 0.23
     if Version(spack.spack_version) < Version("0.23"):
