01855a4b60bb07d1218ea1747027e2c63dc2d643
Thomas Applencourt <tapplencourt@anl.gov>
2025-05-30 16:41:14 +0000

    Update h2yaml

--- packages/h2yaml/package.py
+++ packages/h2yaml/package.py
@@ -11,15 +11,22 @@ class H2yaml(PythonPackage):
 
     version("0.1.1", sha256="7bc695d4aca62baae9708e20351c94154db08a26f7fd31b9b0f2e52bcfd0bf98")
 
-    depends_on("python@3.10:", type=("build", "run"))
+    depends_on("python@3.10:", type=("build", "run", "test"))
     depends_on("py-setuptools", type="build")
-    depends_on("llvm+python", type="run")
-    depends_on("py-pyyaml", type="run")
+    depends_on("llvm+python", type=("run","test"))
+    depends_on("py-pyyaml", type=("run","test"))
     depends_on("py-pytest", type="test")
 
     def setup_run_environment(self, env):
-        env.prepend_path("LD_LIBRARY_PATH", self.spec["llvm"].prefix.lib)       
+        env.prepend_path("LD_LIBRARY_PATH", self.spec["llvm"].prefix.lib)
 
+    def setup_test_environment(self, env):
+        env.prepend_path("LD_LIBRARY_PATH", self.spec["llvm"].prefix.lib)
+
+    def setup_build_environment(self, env):
+        """Wrapper until spack has a real implementation of setup_test_environment()"""
+        if self.run_tests:
+            self.setup_test_environment(env)
 
     @run_after("install")
     @on_package_attributes(run_tests=True)
